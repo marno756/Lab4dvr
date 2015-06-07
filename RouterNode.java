275c29b4a10693384b68caf8fd0e940febde1393
D.S. Ljungmark <spider@aanstoot.se>
2015-06-07 22:06:44 +0200

    Tell our neighbours about our cost estimates
    * Apply some whitespace fixing magic for vim modeline
    * Add an empty cost calculation routine
    * Send the message to our neighbours

--- RouterNode.java
+++ RouterNode.java
@@ -1,4 +1,6 @@
-import javax.swing.*;        
+import javax.swing.*;
+/* vim: set tabstop=4 softtabstop=4 shiftwidth=4 expandtab :*/
+
 
 public class RouterNode {
   private int myID;
@@ -13,26 +15,36 @@ public class RouterNode {
     myGUI =new GuiTextArea("  Output window for Router #"+ ID + "  ");
 
     System.arraycopy(costs, 0, this.costs, 0, RouterSimulator.NUM_NODES);
+    caluclateInitialCost();
 
+    for (int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+        RouterPacket pkt = new RouterPacket(myID, i, costs);
+        sendUpdate(pkt);
+    }
+  }
+
+  //--------------------------------------------------
+  private void caluclateInitialCost() {
+    // Initial Cost Calculation
   }
 
   //--------------------------------------------------
   public void recvUpdate(RouterPacket pkt) {
 
   }
-  
+
 
   //--------------------------------------------------
   private void sendUpdate(RouterPacket pkt) {
     sim.toLayer2(pkt);
 
   }
-  
+
 
   //--------------------------------------------------
   public void printDistanceTable() {
-	  myGUI.println("Current table for " + myID +
-			"  at time " + sim.getClocktime());
+      myGUI.println("Current table for " + myID +
+            "  at time " + sim.getClocktime());
   }
 
   //--------------------------------------------------
